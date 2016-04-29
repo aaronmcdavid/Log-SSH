8f6b9e7cd34aeb9d816d8f6a15d5ea86790dda9e
Aaron McDavid <1acm2@cs.SMRH.NET>
2016-04-29 02:29:06 +0100

    Initial Commit

---
+++ Log-SSH.pl
@@ -0,0 +1,175 @@
+#!/usr/bin/perl -w
+=head1 Log-SSH
+
+=head2 SYNOPSIS
+  
+  Starts a logged SSH session
+
+=head2 DESCRIPTION
+
+  This script is intended as a wrapper for OpenSSH
+
+=head2 INPUTS
+
+  All switches that OpenSSH normally uses as of v7.2p2
+
+=head2 OUTPUTS 
+
+  Executes ssh session and outputs as normal
+  Writes compressed log file to ~/logs/<Hostname>.<DateTime>.log.bz2
+
+=head2 LINK
+
+ https://github.com/aaronmcdavid/Log-SSH
+
+=head2 NOTES
+
+  Version:               1.0.0
+  Author:                Aaron McDavid
+  Author's E-mail:       amcdavid@sheppardmullin.com
+  Original Author:       John Simpson <jms1@jms1.net>
+  Major Version Date:    2016-04-27
+  Current Version Date:  2016-04-27
+  Purpose/Change:        Added syntactic support for all ssh switches
+                         Added bzip compression for logs
+
+=head2 INSTALLATION
+
+  Place this in your ~/bin directory
+	
+  Edit the lines below in the configuration section to match your
+  environment (by default, set for Cygwin)
+	
+  Next, add the following commands to your .bashrc:
+
+    # Automatically log SSH sessions
+    alias ssh='Log-SSH.pl'
+	
+    # Add user executables to path
+    export PATH=$PATH:~/bin
+	
+  Then run the following commands:
+
+    mkdir ~/logs
+    source .bashrc
+
+=head2 LICENSE
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 3, as
+  published by the Free Software Foundation.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
+require 5.003 ;
+use strict ;
+
+###############################################################################
+#
+# Configuration
+
+# the location of your normal "ssh" executable
+my $ssh = "/usr/bin/ssh" ;
+
+# the location of your "tee" executable
+my $tee = "/usr/bin/tee" ;
+
+# the directory where you want the logs to be stored
+# this directory must exist before running the program
+my $logdir = $ENV{"HOME"} . "/logs" ;
+
+# do you want this script to show the real command before running it?
+my $show_cmd = 0 ;
+
+# do you want to compress the log file using bzip2?
+my $compress_log_file_bzip2 = 1;
+
+###############################################################################
+
+###############################################################################
+#
+# Parse arguments, checking for target server and version request
+
+my $target = "" ;
+my $request_version_printout = 0;
+
+
+for my $k ( @ARGV ) {
+	if ( $k =~ /^-[1246AaCfGgKkMNnqsTtvXxYy]*V[1246AaCfGgKkMNnqsTtvXxYy]*$/ ) {
+		# version printout obviates all other switches
+		$request_version_printout = 1 ;
+		}	
+	}
+
+unless ( $request_version_printout ) {
+	# target server is the first non-option argument
+	my $found_value_option = 0 ;
+	for my $k ( @ARGV ) {
+		if ( $found_value_option ) {
+			# previous ARG was an option; this ARG is its value
+			$found_value_option = 0 ;
+			next ;
+			}
+		if ( $k =~ /^-/) {
+			# in OpenSSH 7.2p2, these options don't take a value
+			if ($k !~ /^-[1246AaCfGgKkMNnqsTtVvXxYy]+$/) { 
+				$found_value_option = 1 ;
+				}
+			next ;
+			}
+		$target = $k ;
+		last ;
+		}
+	}
+
+	if ( $target =~ /\@(.*)$/ ) {
+	# if username was specified with @ syntax, use only the hostname
+	$target = $1 ;
+	}
+
+# re-escape arguments
+for (0..$#ARGV) {
+	$ARGV[$_] =~ s|([^a-zA-Z0-9,._+@%/\-])|\\$1|g ;			
+	}
+	
+# prepare base cmd
+my $cmd = "$ssh " . join ( " " , @ARGV ) ;
+
+# if we can't find a target, neither will ssh
+# in this case, don't log anything; just output what ssh outputs
+unless ( $target ) {
+	exec $cmd ;
+	}
+
+# construct logfile name
+$target = lc $target ;
+$target =~ s#([<>:"/\\|?*])#_#g ;		# strip out invalid characters
+my @d = localtime ;
+my $now = sprintf ( "%04d-%02d-%02d_%02d.%02d.%02d" ,
+	$d[5]+1900 , $d[4]+1 , $d[3] , $d[2] , $d[1] , $d[0] ) ;
+my $logfile = $logdir . "/${target}.${now}.log" ;
+
+
+# add logging to our command
+$cmd .= " | $tee -a $logfile" ;
+
+# print header to logfile
+open ( L , ">$logfile" )
+	or die "Can't create logfile \"$logfile\": $!\n" ;
+print L "CMD: $cmd\n\n" , "=" x 78 , "\n" ;
+close L ;
+
+# execute ssh
+$show_cmd && print "% $cmd\n" ;
+system $cmd and die ;
+
+#compress log file
+$compress_log_file_bzip2 and system "bzip2 -9 '${logfile}'" ;
